a4251603723cbe83267f43974773fb86635d8741
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-23 01:17:33 +0700

    download-capcut.js

--- plugins/download-capcut.js
+++ plugins/download-capcut.js
@@ -21,7 +21,7 @@ const plugin = {
         const replyMarkup = {
           reply_markup: {
             inline_keyboard: [
-              [{ text: 'Script Bot', url: buttonUrl }],
+              [{ text: 'WhatsApp Group', url: buttonUrl }],
             ],
           },
         };
