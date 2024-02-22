259b81afd1eb37e25986114f2d1bece28e12642b
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-23 01:19:27 +0700

    download-tiktok.js

--- plugins/download-tiktok.js
+++ plugins/download-tiktok.js
@@ -32,7 +32,7 @@ const plugin = {
         const replyMarkup = {
           reply_markup: {
             inline_keyboard: [
-              [{ text: 'Script Bot', url: buttonUrl }],
+              [{ text: 'WhatsApp Group', url: buttonUrl }],
               [{ text: 'Audio', url: audio }],
             ],
           },
