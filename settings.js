85caf065fb0b34e59cf9cd81294c19f4ddcac491
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-03-01 14:45:33 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ module.exports = {
   ownerUsernames: ["@iky_reallysad"],//Your Username
   ownerNumber: "6285794908894",//Your Number 
   telegramBotToken: "6916462809:AAFwx3iiv66aCFRDV-QCWJmFSf3TNY_rcn0",//Your Bot Tokens
-  imageUrl: "https://btch.pages.dev/file/9e4162d4034241953fdfb.jpg",//Thumbnail Url
-  buttonUrl: "https://chat.whatsapp.com/GbNiwLK3R9y4pOUFDW5eAQ",//Script Url
-  api: "https://api.botcahx.eu.org",
-  apikey: "ikyalwaysgood",//Change Your Apikey
+  imageUrl: "https://telegra.ph/file/5a9d33e6daff292833283.jpg",//Thumbnail Url
+  buttonUrl: "https://www.instagram.com/iky_alwaysgood?igsh=MXdwbXR3a2xxNjRocA==",//Script Url
+  api: "https://api.betabotz.eu.org",
+  apikey: "Rn9sFXsY",//Change Your Apikey
   mess: {
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
